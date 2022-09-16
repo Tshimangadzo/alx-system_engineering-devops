76cb22f997e4a5012f26233b6637ee20c9f94b86
Tshimangadzo Mudznani <mudzanani27@gmail.com>
2022-09-16 09:29:56 +0200

    modified 0-create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,8 @@
 file { 'ntp.conf':
   path    => '/tmp/school',
   mode => 0744,
-  owner =>  "www-data"
-  group =>  "www-data"
+  owner =>  'www-data',
+  group =>  'www-data'
 }
 
 # File contains I love Puppet
